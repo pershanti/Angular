0452bf823e79331d11eff6acc65a1d5d26ad7460
pershanti <shantini29@gmail.com>
2017-10-16 16:31:41 -0700

    fixed ngif

--- emailApp/src/app/app.component.ts
+++ emailApp/src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Email List';
   emailList: any = [
-      { name: 'Bob', email: 'BOB@email.com', importance: 'high', subject: 'hi!', content: 'hey I just wanted to let you know' },
-      { name: 'Anna', email: 'anna@banana.com' , importance: 'high', subject: 'hello!', content: 'hey I just wanted to let you know' },
-      { name: 'Coding', email: 'coding@dojo.com' , importance: 'high', subject: 'hola!', content: 'hey I just wanted to let you know' }
+      { name: 'Bob', email: 'BOB@email.com', importance: false, subject: 'hi!', content: 'hey I just wanted to let you know' },
+      { name: 'Anna', email: 'anna@banana.com' , importance: true, subject: 'hello!', content: 'hey I just wanted to let you know' },
+      { name: 'Coding', email: 'coding@dojo.com' , importance: true, subject: 'hola!', content: 'hey I just wanted to let you know' }
     ];
 }
