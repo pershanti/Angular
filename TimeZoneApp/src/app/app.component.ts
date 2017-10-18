763ba58a927f00e72093efda5aafa8b4c2268a0f
pershanti <shantini29@gmail.com>
2017-10-18 13:47:09 -0700

    finished timezone app

--- TimeZoneApp/src/app/app.component.ts
+++ TimeZoneApp/src/app/app.component.ts
@@ -13,31 +13,32 @@ export class AppComponent {
   switchC = false;
   switchE = false;
 
-  clear(){
+  clear() {
     this.time = null;
-    this.switchP = true;
-    this.switchM = true;
-    this.switchC = true;
-    this.switchE = true;
-  function display(timezone) {
+    this.switchP = false;
+    this.switchM = false;
+    this.switchC = false;
+    this.switchE = false;
+  }
+  display(timezone) {
     if (timezone === 'PST') {
       this.switchP = true;
-      const offset = -8;
+      const offset = -7;
       this.time = new Date(new Date().getTime() + offset * 3600 * 1000).toUTCString().replace(/ GMT$/, '');
     }
     if (timezone === 'MST') {
       this.switchM = true;
-      const offset = -7;
+      const offset = -6;
       this.time = new Date(new Date().getTime() + offset * 3600 * 1000).toUTCString().replace(/ GMT$/, '');
     }
     if (timezone === 'CST') {
       this.switchC = true;
-      const offset = -6;
+      const offset = -5;
       this.time = new Date(new Date().getTime() + offset * 3600 * 1000).toUTCString().replace(/ GMT$/, '');
     }
     if (timezone === 'EST') {
       this.switchE = true;
-      const offset = -5;
+      const offset = -4;
       this.time = new Date(new Date().getTime() + offset * 3600 * 1000).toUTCString().replace(/ GMT$/, '');
     }
   }
