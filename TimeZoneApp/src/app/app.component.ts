28d441d648f145681a20e771aa72eba624624b54
pershanti <shantini29@gmail.com>
2017-10-18 13:13:59 -0700

    created timezone app

---
+++ TimeZoneApp/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  time;
+  switchP = false;
+  switchM = false;
+  switchC = false;
+  switchE = false;
+
+  clear(){
+    this.time = null;
+    this.switchP = true;
+    this.switchM = true;
+    this.switchC = true;
+    this.switchE = true;
+  function display(timezone) {
+    if (timezone === 'PST') {
+      this.switchP = true;
+      const offset = -8;
+      this.time = new Date(new Date().getTime() + offset * 3600 * 1000).toUTCString().replace(/ GMT$/, '');
+    }
+    if (timezone === 'MST') {
+      this.switchM = true;
+      const offset = -7;
+      this.time = new Date(new Date().getTime() + offset * 3600 * 1000).toUTCString().replace(/ GMT$/, '');
+    }
+    if (timezone === 'CST') {
+      this.switchC = true;
+      const offset = -6;
+      this.time = new Date(new Date().getTime() + offset * 3600 * 1000).toUTCString().replace(/ GMT$/, '');
+    }
+    if (timezone === 'EST') {
+      this.switchE = true;
+      const offset = -5;
+      this.time = new Date(new Date().getTime() + offset * 3600 * 1000).toUTCString().replace(/ GMT$/, '');
+    }
+  }
+}
